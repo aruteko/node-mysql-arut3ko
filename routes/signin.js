edbcfc0742f7cce15626d19b8d562d5e092b0b52
Taichi_Sato_Matsue_Kosen <114867136+aruteko@users.noreply.github.com>
2025-07-10 05:28:53 +0000

    18章

--- routes/signin.js
+++ routes/signin.js
@@ -1,55 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const knex = require("../db/knex");
-const bcrypt = require("bcrypt");
+const passport = require("passport");
 
 router.get('/', function (req, res, next) {
   const userId = req.session.userid;
   const isAuth = Boolean(userId);
-  res.render('signin', {
+  res.render("signin", {
+    title: "Sign in",
     isAuth: isAuth,
-    title: 'Sign in',
   });
 });
 
-router.post('/', function (req, res, next) {
-  const username = req.body.username;
-  const password = req.body.password;
-  const userId = req.session.userid;
-  const isAuth = Boolean(userId);
-
-  knex("users")
-    .where({
-    name: username,
-    })
-    .select("*")
-    .then(async function (results) {
-      if (results.length === 0) {
-        res.render("signin", {
-          title: "Sign in",
-          errorMessage: ["ユーザが見つかりません"],
-          isAuth: isAuth,
-        });
-      } else if (await bcrypt.compare(password, results[0].password)) {
-        req.session.userid = results[0].id;
-        res.redirect('/');
-      } else {
-        res.render("signin", {
-          title: "Sign in",
-          errorMessage: ["ユーザが見つかりません"],
-          isAuth: isAuth,
-        });
-      }
-    })
-    .catch(function (err) {
-      console.error(err);
-      res.render("signin", {
-        isAuth: isAuth,
-        title: "Sign in",
-        errorMessage: [err.sqlMessage],
-        isAuth: false,
-      });
-    });
-});
+router.post('/', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/signin',
+    failureFlash: false,
+  }
+));
 
 module.exports = router;
